e8e1522d153824bbc8dd9abecf3d66e6a2241b61
Meike <meike_hankewicz@yahoo.de>
2017-11-30 20:10:27 +0100

    quote amount functionality

--- app.js
+++ app.js
@@ -10,55 +10,48 @@ app.get("/", function(req, res){
 
 // Connect to Database
 MongoClient.connect("mongodb://localhost/quotesAPI", function(err, db){
-    var quotes = db.collection('quotes');
+    var quotes = db.collection('quotes');      
 
     // Get Random Quotes
     app.get("/quotes", function(req, res){
-        // Check For Query Limit
-        if(req.query.limit){
-            var limit = Number(req.query.limit);
-            quotes.aggregate([{$sample: {size: limit}}, {$project: {_id: 0}}], function(err, items){
-                res.send(items);
-            });
-        } else {
-            quotes.findOne({"character": "The Doctor"}, function(err, item){
-                if(err){
-                    res.send("Something is wrong");
-                } else{
-                    res.send(item);
-                }
-            });
-        }
-    });
-
-    // Get Quotes By Character
-    app.get("/quotes/:character", function(req, res){
-        quotes.find({"character": req.params.character}).toArray(function(err, items){
+        quoteAmount(req);
+        quotes.aggregate([{$sample: {size: amount}}, {$project: {_id: 0}}], function(err, items){
             if(err){
-                res.send("Something is wrong");
-            } else{
+                res.send("Sorry, something went wrong! Error:" + err);
+            } else {
                 res.send(items);
             }
         });
     });
 
+    // Get Quotes By Character
+    app.get("/quotes/:character", function(req, res){
+        quoteAmount(req);
+        quotes.aggregate([
+            {$match: {"character": req.params.character}},
+            {$sample: {size: amount}},
+            {$project: {_id: 0}}
+        ], function(err, items){
+            if(err){
+                res.send("Sorry, something went wrong! Error:" + err);
+            } else {
+                res.send(items);
+            }
+        });        
+    });
+
+    // Check Amount Of Quotes
+    function quoteAmount(req){
+        if(req.query.amount){
+            return amount = Number(req.query.amount);            
+        } else {
+            return amount = 1;
+        };
+    };
+          
 });
 
 // Listen To Port
 app.listen(port, function(){
-    console.log('API server has started');
+    console.log('API server has started. Listening on port ' + port);
 });
-
-
-// Character Filter
-/*
-if(req.query.character){
-    var character = req.query.character;
-    quotes.findOne({"character": character}, function(err, item){
-        if(err){
-            res.send("Something is wrong");
-        } else{
-            res.send(item);
-        }                    
-    });
-} else{}*/
